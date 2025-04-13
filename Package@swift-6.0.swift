8645b3b6b5a16bb820fb3b04c9ea3222499eddda
tatsubee <tatsubee@pixiv.co.jp>
2025-04-13 19:15:35 +0900

    multiple swift language modes

--- Package@swift-6.0.swift
+++ Package@swift-6.0.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 
 import CompilerPluginSupport
 import PackageDescription
@@ -26,5 +26,6 @@ let package = Package(
             name: "UIEnvironmentTests",
             dependencies: ["UIEnvironment"]
         ),
-    ]
+    ],
+    swiftLanguageModes: [.v6]
 )
